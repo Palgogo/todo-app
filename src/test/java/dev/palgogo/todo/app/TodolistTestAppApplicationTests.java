91aa48f7db1f1224db474574e013c8de13c630ec
Pavel Maisiuk-Dranko <ipalgogo@gmail.com>
2021-04-16 17:29:49 +0300

    Initial commit

---
+++ src/test/java/dev/palgogo/todo/app/TodolistTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.palgogo.todo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistTestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
