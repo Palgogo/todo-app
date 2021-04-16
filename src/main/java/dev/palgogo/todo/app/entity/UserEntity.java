1813d4dc7748cf99fd16760c55c230e5c56b17fb
Pavel Maisiuk-Dranko <ipalgogo@gmail.com>
2021-04-16 22:03:37 +0300

    feature/create-task: add users entity.

---
+++ src/main/java/dev/palgogo/todo/app/entity/UserEntity.java
@@ -0,0 +1,35 @@
+package dev.palgogo.todo.app.entity;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    private UUID id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "department_id")
+    private UUID departmentId;
+}
