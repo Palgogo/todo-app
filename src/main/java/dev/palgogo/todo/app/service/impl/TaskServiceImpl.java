4faef8b2ca73ea7f0b5a7be19d190cf5a34db2bd
Pavel Maisiuk-Dranko <ipalgogo@gmail.com>
2021-04-18 20:27:26 +0300

    feature/update-status: add update status endpoint.

--- src/main/java/dev/palgogo/todo/app/service/impl/TaskServiceImpl.java
+++ src/main/java/dev/palgogo/todo/app/service/impl/TaskServiceImpl.java
@@ -2,6 +2,7 @@ package dev.palgogo.todo.app.service.impl;
 
 import dev.palgogo.todo.app.dto.CreateTaskRequest;
 import dev.palgogo.todo.app.dto.TaskDto;
+import dev.palgogo.todo.app.dto.UpdateStatusRequest;
 import dev.palgogo.todo.app.entity.TaskEntity;
 import dev.palgogo.todo.app.entity.TaskStatus;
 import dev.palgogo.todo.app.mapper.TaskMapper;
@@ -9,6 +10,9 @@ import dev.palgogo.todo.app.repository.TaskRepository;
 import dev.palgogo.todo.app.service.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +26,15 @@ public class TaskServiceImpl implements TaskService {
         TaskEntity taskEntity = save(entity);
 
         return taskMapper.toTaskDto(taskEntity);
+    }
+
+    @Override
+    public void updateStatus(UUID id, UpdateStatusRequest updateStatusRequest) {
+        Optional<TaskEntity> entityOptional = taskRepository.findById(id);
+        entityOptional.ifPresent(taskEntity -> {
+            taskEntity.setStatus(updateStatusRequest.getStatus());
+            taskRepository.save(taskEntity);
+        });
     }
 
     private TaskEntity save(TaskEntity entity) {
